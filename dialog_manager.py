1405355f67cf1a7d86fd9316be7fe72d3a912ba6
David Dale <ddale@yandex-team.ru>
2019-02-02 16:31:40 +0300

    use basic_nlu from tgalice

--- dialog_manager.py
+++ dialog_manager.py
@@ -1,6 +1,7 @@
 import pickle
 import copy
 from tgalice.dialog_manager import BaseDialogManager
+from tgalice import basic_nlu
 
 from pushkin.rhyme import Rhymer
 
@@ -19,22 +20,16 @@ COMMAND_EXIT = 'exit'
 
 class StupidDialogManager(BaseDialogManager):
     def respond(self, user_object, message_text):
-        suggests = []
+        suggests = ['довольно']
         updated_user_object = copy.deepcopy(user_object)
         commands = []
-        text = message_text.lower()
-        if not text or text == 'помощь' or not updated_user_object:
+        text = basic_nlu.fast_normalize(message_text)
+        if not text or basic_nlu.like_help(text) or not updated_user_object:
             response = TEXT_HELP
-        elif text == 'довольно' or text == 'алиса хватит':
+        elif text == 'довольно' or basic_nlu.like_exit(text):
             response = TEXT_FAREWELL
             commands.append(COMMAND_EXIT)
         else:
             response = rhymer.random_rhyme(text)
-        """
-        if message_text:
-            response = "Вы сказали, '{}'".format(message_text.lower())
-        else:
-            response = "Вы не сказали ничего!"
-        """
         updated_user_object['last_dialog'] = [text, response]
         return updated_user_object, response, suggests, commands
